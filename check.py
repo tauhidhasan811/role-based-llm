4128d42903a8b69500d962dc8e046d08aa95aa39
tauhidhasan811 <tauhidhasan811@gmail.com>
2025-11-18 15:31:23 +0600

    add prompt

--- check.py
+++ check.py
@@ -1,3 +1,18 @@
+from dotenv import load_dotenv
 from component.services.prompt import prompt_generate
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
 
-print(prompt_generate('doctor'))
+load_dotenv()
+prompt = prompt_generate(user_prompt='How are you',user_role='doctor')
+
+llm = HuggingFaceEndpoint(
+    repo_id="openai/gpt-oss-120b",
+    task='text-generation'
+)
+
+model = ChatHuggingFace(llm=llm)
+
+response = model.invoke(prompt)
+print(f"\n\nResponse is : {response}")
+
+print(f"\n\Content is : {response.content}\n\n")
--- component/services/prompt.py
+++ component/services/prompt.py
@@ -2,7 +2,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 
-def prompt_generate(user_prompt):
+def prompt_generate(user_prompt, user_role):
 
     systemMessage = SystemMessage(
             content="Consider you are a chatbot based on user role. You will Answer based whic is more relevant or related with that spacific role."
@@ -11,10 +11,11 @@ def prompt_generate(user_prompt):
         content=f"{user_prompt}"
         )
     temp = PromptTemplate(
-        template="{system_message}{human_message}",
-        input_variables=['system_message', 'human_message']
+        template="{system_message} User role {user_role} user Question: {human_message}",
+        input_variables=['system_message', 'human_message', 'user_role']
         )
     prompt = temp.invoke(input={'system_message': systemMessage, 
-                                'human_message': humanMessage})
+                                'human_message': humanMessage,
+                                'user_role': user_role})
     
     return prompt
